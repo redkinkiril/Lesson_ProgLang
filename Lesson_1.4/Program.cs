2830a670e40aa9b71260c619cb3e4936ecd7b31b
redkinkiril <redkinkiril@yandex.ru>
2022-09-01 18:51:09 +0300

    added program code

---
+++ Lesson_1.4/Program.cs
@@ -0,0 +1,24 @@
+﻿// See added program that takes a number (N) as input, and shows all even numbers from 1 to N as output.
+
+int i = 1;
+bool not = true;
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + num);
+while (i <= num)
+{
+if (i % 2 != 1)
+{
+Console.Write(i + ", ");
+not = false;
+}
+i++;
+}
+
+if (not)
+{
+ Console.WriteLine("Нет чётных чисел!");
+}
+
