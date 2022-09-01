e798e05ea29851d5f044677a0df143484fd04669
redkinkiril <redkinkiril@yandex.ru>
2022-09-01 18:45:59 +0300

    added program code

---
+++ Lesson_1.3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See added program that takes a number as input and outputs whether the number is even (whether it is divisible by two without remainder).
+
+Console.WriteLine("Введите число:");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 1)
+{
+    Console.WriteLine("Число " + num + "является: НЕЧЁТНЫМ");
+}
+else
+{
+    Console.WriteLine("Число " + num + "является: ЧЁТНЫМ");
+}
