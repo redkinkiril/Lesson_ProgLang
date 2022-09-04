d7bfc319c32435fbe1dbe23ede3fff214a5e1b18
redkinkiril <redkinkiril@yandex.ru>
2022-09-04 16:21:55 +0300

    added program code Lesson_2.3

---
+++ Lesson_2.3/Program.cs
@@ -0,0 +1,28 @@
+﻿// See added program that accepts a digit indicating the day of the week as input and checks whether this day is a weekend.;
+
+Console.WriteLine("enter the number of the day of the week: ");
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+string  isWeekend= (dayOfWeek  >0 && dayOfWeek  <8) 
+    ? (dayOfWeek == 6 || dayOfWeek == 7) ? "yes" : "no"
+    : "not day of the week";
+
+Console.WriteLine($"{dayOfWeek} -> {isWeekend}");
+
+
+/*
+if(dayOfWeek > 0 && dayOfWeek < 8) 
+{
+  if(dayOfWeek == 6 || dayOfWeek == 7) 
+  {
+    Console.WriteLine($"{dayOfWeek} -> yes");
+  } 
+  else 
+  {
+    Console.WriteLine($"{dayOfWeek} -> no");
+  }
+  else
+  {
+    Console.WriteLine($"{dayOfWeek} -> not day of the week");  
+  }
+}
+*/
