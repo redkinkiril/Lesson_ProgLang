2a1cbedf7b867a2fa1ef670bfdfd0519225b29f0
redkinkiril <redkinkiril@yandex.ru>
2022-09-04 16:08:30 +0300

    added program code in Lesson_2.2

--- Lesson_2.2/Program.cs
+++ Lesson_2.2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information;
+﻿// See added a program that outputs the third digit of a given number or reports that there is no third digit.;
 
+Console.WriteLine("enter a number:");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberS = Math.Abs(number).ToString();
+
+Console.WriteLine(numberS.Length > 2 ? $"{number} -> {numberS[2]}" : $"{numberS} -> there is no third digit");
+
+/*
+if(numberS.Length > 2) {
+ Console.WriteLine($"{number} -> {numberS[2]}");
+} else {
+ Console.WriteLine($"{number} -> there is no third digit");
+}
+*/
