31aaeb7686ad3eb054e9836385a70f9cbc8ac324
redkinkiril <redkinkiril@yandex.ru>
2022-09-04 17:14:30 +0300

    added program code in Lesson_2.4

---
+++ Lesson2.4/Program.cs
@@ -0,0 +1,9 @@
+﻿// See added program that accepts an integer of any bit depth as input and deletes 
+//the second digit from left to right of this number;
+
+Console.WriteLine("enter a number:");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+
