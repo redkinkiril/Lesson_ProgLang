369cce8bd0cd822e188c7c83bd3e2af0aeb0cd09
redkinkiril <redkinkiril@yandex.ru>
2022-09-07 15:22:38 +0300

    added change code

--- Lesson_2.1/Program.cs
+++ Lesson_2.1/Program.cs
@@ -1,9 +1,15 @@
 ﻿// See added program that accepts a three-digit number as input and shows the second digit of this number as output.  
 
-Console.WriteLine("enter a three-digit number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-string numberS = Math.Abs(number).ToString();
-Console.WriteLine(numberS.Length == 3 ? $"{number} -> {numberS[1]}" : "you entered a non-three-digit number");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("enter a three-digit number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        string numberS = Math.Abs(number).ToString();
+        Console.WriteLine(numberS.Length == 3 ? $"{number} -> {numberS[1]}" : "you entered a non-three-digit number");
+    }
+}
 
 /* if(numberS.Length != 3) 
     {
