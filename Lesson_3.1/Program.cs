792459d3144ce6eef0d25e5bebd0c6074ad07d21
redkinkiril <redkinkiril@yandex.ru>
2022-09-09 23:09:26 +0300

    added program code Lesson_3.1

---
+++ Lesson_3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See added Write a loop that takes two numbers (A and B) as input and raises the number A to the natural power of B.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int Exponentiation(int numberA, int numberB)
+        {
+            int result = 1;
+            for (int i = 1; i <= numberB; i++)
+            {
+                result = result * numberA;
+            }
+            // int result = Math.Pow(numberA, numberB);
+            return result;
+        }
+
+        Console.Write("Enter number A: ");
+        int numberA = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter number B: ");
+        int numberB = Convert.ToInt32(Console.ReadLine());
+        int exponentiation = Exponentiation(numberA, numberB);
+        Console.WriteLine("Answer: " + exponentiation);
+    }
+}
