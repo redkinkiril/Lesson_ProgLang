ac2cc818f31db5d3a21d26fadb4dd3eabc89edc6
redkinkiril <redkinkiril@yandex.ru>
2022-09-09 23:30:53 +0300

    added program code in Lesson_3.3

---
+++ Lesson_3.3/Program.cs
@@ -0,0 +1,4 @@
+﻿// See added Write a program that sets an array of N elements and displays them on the screen. 
+// N - entered from the keyboard.
+// Elements are also entered from the keyboard.
+
