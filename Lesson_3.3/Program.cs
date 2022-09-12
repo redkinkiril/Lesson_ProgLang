0fbb262a0e5a90a01e134f44fb173d5fb2d37396
redkinkiril <redkinkiril@yandex.ru>
2022-09-12 18:14:47 +0300

    added change code in Lesson_3.3

--- Lesson_3.3/Program.cs
+++ Lesson_3.3/Program.cs
@@ -7,12 +7,12 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.Write("Введите ряд чисел, разделенных запятой : ");
+        Console.Write("enter a number : ");
         string? seriesOfNumbers = Console.ReadLine();
 
-        seriesOfNumbers = seriesOfNumbers + ","; // дополнительня запятая для обозначения конца строки
+        seriesOfNumbers = seriesOfNumbers + ","; // an additional comma to indicate the end of the line
 
-        // функция удаления пробелов из строки 
+        // function for removing spaces from a string 
         string RemovingSpaces(string series)
         {
             string seriesNew = "";
@@ -26,52 +26,46 @@ internal class Program
             return seriesNew;
         }
 
-        // функция проверки на правильность ввода 
+        // function of checking for the correctness of the input
         void СheckNumber2(int series)
         {
 
             if (series == '0' || series == '1' || series == '2'
             || series == '3' || series == '4' || series == '5' || series == '6'
             || series == '7' || series == '8' || series == '9' || series == ','
-            || series == '-')
-            {
-            }
+            || series == '-');
+        
             else
             {
-                Console.WriteLine($"Ошибка ввода  символа. Вводи цифры.");
+                Console.WriteLine($"Character input error. Enter the numbers.");
 
             }
         }
 
-        // функция  создания и заполнения массива из строки
+        // function of creating and filling an array from a string
         int[] ArrayOfNumbers(string seriesNew)
         {
-
-            int[] arrayOfNumbers = new int[1];    // инициализация массива из 1 элемента
-
+            int[] arrayOfNumbers = new int[1];    // initializing an array of 1 element
             int j = 0;
-
             for (int i = 0; i < seriesNew.Length; i++)
             {
                 string seriesNew1 = "";
-
                 while (seriesNew[i] != ',' && i < seriesNew.Length)
                 {
                     seriesNew1 += seriesNew[i];
                     СheckNumber2(seriesNew[i]);
                     i++;
                 }
-                arrayOfNumbers[j] = Convert.ToInt32(seriesNew1);    // заполняет массив значениями из строки
+                arrayOfNumbers[j] = Convert.ToInt32(seriesNew1);    // fills the array with values from the string
                 if (i < seriesNew.Length - 1)
                 {
-                    arrayOfNumbers = arrayOfNumbers.Concat(new int[] { 0 }).ToArray();    // добавляет новый нулевой элемент в конец массива
+                    arrayOfNumbers = arrayOfNumbers.Concat(new int[] { 0 }).ToArray();    // adds a new null element to the end of the array
                 }
                 j++;
             }
             return arrayOfNumbers;
         }
-
-        // функция  вывода массива на печать 
+        // array output function for printing 
         void PrintArry(int[] coll)
         {
             int count = coll.Length;
@@ -88,8 +82,7 @@ internal class Program
             }
             Console.Write("]");
         }
-
-
+        
         string seriesNew = RemovingSpaces(seriesOfNumbers);
 
         int[] arrayOfNumbers = ArrayOfNumbers(seriesNew);
---
+++ Lesson_3.4/Program.cs
@@ -0,0 +1,21 @@
+﻿// See added program that accepts a number (integer or real) as input and outputs the number of digits in the number.
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        long i = 0;
+        decimal n;
+        Console.Write("Enter number: ");
+        n = Convert.ToInt64(Console.ReadLine());
+        
+        do
+        {
+            n = n / 10;
+            i++;
+        }
+        while(Math.Abs(n) >= 1);
+        Console.WriteLine("number of elements: " + (i));
+    }
+}
