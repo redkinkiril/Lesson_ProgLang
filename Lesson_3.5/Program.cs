e5c07c08afe68f59892ef0340a544b6acd05cf18
redkinkiril <redkinkiril@yandex.ru>
2022-09-13 19:13:04 +0300

    added change file

---
+++ Lesson_3.5/Program.cs
@@ -0,0 +1,22 @@
+﻿// See 
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = new int[8];
+        Console.Write("[");
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(0, 11);
+            Console.Write(" " + Method(i) + " ");
+        }
+        Console.Write("]");
+
+        int Method(int a)
+        {
+            return numbers[a];
+        }
+    }
+}
