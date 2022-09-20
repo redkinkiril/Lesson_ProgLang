ee078626502152bbf9c2806ea790be79be110c4a
redkinkiril <redkinkiril@yandex.ru>
2022-09-20 14:33:07 +0300

    added change Lesson_9.1

---
+++ Lesson_9.1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Set the values of M and N. Write a program that outputs all natural numbers in the range from M to N.
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = InputInt("Введите положительное число");
+        int m = 1;
+        if (n < 1)
+        {
+            Console.WriteLine("Ввели не положительное число");
+        }
+        Console.WriteLine(NaturalNumber(n, m));
+
+        int NaturalNumber(int n, int m)
+        {
+            if (n == m)
+                return n;
+            else
+                Console.Write($"{NaturalNumber(n, m + 1)}, ");
+            return m;
+        }
+
+        int InputInt(string output)
+        {
+            Console.Write(output);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
