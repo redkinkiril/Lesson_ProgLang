4a196f93e64c0c13c679339c3545ced3b78f1da8
redkinkiril <redkinkiril@yandex.ru>
2022-09-20 14:39:08 +0300

    added change Lesson_9.3

---
+++ Lesson_9.3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Write a program for calculating the Ackerman function using recursion. Two non-negative numbers m and n are given.
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите два положительных числа: M и N.");
+        int m = InputInt("Введите M: ");
+        int n = InputInt("Введите N: ");
+        Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+
+        int InputInt(string output)
+        {
+            Console.Write(output);
+            return int.Parse(Console.ReadLine());
+        }
+
+        int Akkerman(int m, int n)
+        {
+            if (m == 0)
+                return n + 1;
+            if (m > 0 && n == 0)
+                return Akkerman(m - 1, 1);
+            else
+                return Akkerman(m - 1, Akkerman(m, n - 1));
+        }
+    }
+}
