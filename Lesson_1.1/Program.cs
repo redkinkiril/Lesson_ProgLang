8f2dcca71d9e4581caaf31802695727252df1bb9
redkinkiril <redkinkiril@yandex.ru>
2022-09-20 12:12:36 +0300

    added englich txt

--- Lesson_1.1/Program.cs
+++ Lesson_1.1/Program.cs
@@ -1,16 +1,16 @@
 ﻿// See add prog_1.1 -> program that accepts two numbers as input and outputs which number is greater and which is smaller.
 
-Console.WriteLine("Введите первое число:");
+Console.WriteLine("Enter first number:");
 int num_A = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите второе число:");
+Console.WriteLine("Enter second number:");
 int num_B = Convert.ToInt32(Console.ReadLine());
 
 if (num_A > num_B)
 {
-    Console.WriteLine(" Первое число " + num_A + " больше чем второе " + num_B);
+    Console.WriteLine(" First number " + num_A + " more than the second " + num_B);
 }
 else
 {
-    Console.WriteLine(" Второе число " + num_B + " больше чем первое " + num_A);
+    Console.WriteLine(" Second number " + num_B + " more than the first " + num_A);
 }
