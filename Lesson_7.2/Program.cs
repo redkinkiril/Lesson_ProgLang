9c44f7be17cfebe764f06be1a3da01fad704d712
redkinkiril <redkinkiril@yandex.ru>
2022-09-23 11:58:04 +0300

    added program code Lesson_7.2

--- Lesson_7.2/Program.cs
+++ Lesson_7.2/Program.cs
@@ -1,51 +1,45 @@
 ﻿// Write a program that takes the positions of an element in a two-dimensional array as input,
 // and returns the value of this element or an indication that there is no such element.
 
-internal partial class Program
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+FillArrayRandomNumbers(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
 {
-    private static void Main(string[] args)
+ Console.WriteLine("такого элемента нет");
+}
+else
+{
+ Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+}
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+ for (int i = 0; i < array.GetLength(0); i++)
+        {        
+ for (int j = 0; j < array.GetLength(1); j++)
+            {
+ array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+ for (int i = 0; i < array.GetLength(0); i++)
     {
-        Console.WriteLine("введите номер строки");
-        int n = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("введите номер столбца");
-        int m = Convert.ToInt32(Console.ReadLine());
-        int[,] numbers = new int[10, 10];
-        FillArrayRandomNumbers(numbers);
-
-        if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+ Console.Write("[ ");
+ for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.WriteLine("такого элемента нет");
-        }
-        else
-        {
-            Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n - 1, m - 1]}");
-        }
-
-        PrintArray(numbers);
-
-        void FillArrayRandomNumbers(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    array[i, j] = new Random().Next(-100, 100) / 10;
-                }
-            }
-        }
-
-        void PrintArray(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                Console.Write("[ ");
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.Write(array[i, j] + " ");
-                }
-                Console.Write("]");
-                Console.WriteLine("");
-            }
-        }
+ Console.Write(array[i,j] + " ");
+        }   
+ Console.Write("]");
+        Console.WriteLine(""); 
     }
 }
