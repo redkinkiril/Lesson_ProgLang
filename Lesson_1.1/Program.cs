90fe0d00f32daacc3e7cc16fc3151be7e7c3505d
redkinkiril <redkinkiril@yandex.ru>
2022-09-23 12:06:35 +0300

    added change code Lesson_7.3

--- Lesson_1.1/Program.cs
+++ Lesson_1.1/Program.cs
@@ -1,16 +1,22 @@
 ﻿// See add prog_1.1 -> program that accepts two numbers as input and outputs which number is greater and which is smaller.
 
-Console.WriteLine("Enter first number:");
-int num_A = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Enter second number:");
-int num_B = Convert.ToInt32(Console.ReadLine());
-
-if (num_A > num_B)
+internal class Program
 {
-    Console.WriteLine(" First number " + num_A + " more than the second " + num_B);
-}
-else
-{
-    Console.WriteLine(" Second number " + num_B + " more than the first " + num_A);
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first number:");
+        int num_A = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter second number:");
+        int num_B = Convert.ToInt32(Console.ReadLine());
+
+        if (num_A > num_B)
+        {
+            Console.WriteLine(" First number " + num_A + " more than the second " + num_B);
+        }
+        else
+        {
+            Console.WriteLine(" Second number " + num_B + " more than the first " + num_A);
+        }
+    }
 }
