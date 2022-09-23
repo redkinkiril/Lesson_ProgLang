7dcd22b89726724878fd2c6b437a618f451bcf8d
redkinkiril <redkinkiril@yandex.ru>
2022-09-23 18:51:13 +0300

    added change code Lesson_8.2

--- Lesson_8.2/Program.cs
+++ Lesson_8.2/Program.cs
@@ -1,56 +1,65 @@
 ﻿// Specify a rectangular two-dimensional array. Write a program that will find the string with the smallest sum of elements.
 
-internal partial class Program
+Console.WriteLine($"\nВведите размер массива m x n и диапазон случайных значений:");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
 {
-    private static void Main (string [] args)
+  int tempSumLine = SumLineElements(array, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        Console.WriteLine("введите размер квадратного массива");
-        int = Convert.32 (Console.ReadLine());
-        int[,] numbers = new int[massVol, massVol];
-        FillArrayRandomNumbers(numbers);
-        PrintArray(numbers);
-        int = int.MaxValue;
-        0 = 0;
-
-        for (int i = 0; i < numbers.GetLength(0); i++)
-        {
-            0  = 0;
-            for (int j = 0; j < numbers.GetLength(1); j++)
-            {
-                sum  + sum  = numbers [i, j];
-            }
-            if (sum < minsum)
-            {
-                minsum  = sum;
-                ++;
-            }
-        }
-
-        Console.WriteLine("строка с наименьшей суммой елементов под номером: " + indexLine + ", с суммой елементов равной: " + minsum);
-
-        void FillArrayRandomNumbers(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    array[i, j] = new Random().Next(0, 10);
-                }
-            }
-        }
-
-        void PrintArray(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                Console.Write("[ ");
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.Write(array[i, j] + " ");
-                }
-                Console.Write("]");
-                Console.WriteLine("");
-            }
-        }
+      array[i, j] = new Random().Next(range);
     }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
 }
